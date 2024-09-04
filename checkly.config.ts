07aa41736755845e16a8603022c24c5d7b8233d8
Corentin Lumineau <corentin.lumineau@e-xpertsolutions.com>
2024-09-04 12:16:01 +0200

    feat: update checkly config

--- checkly.config.ts
+++ checkly.config.ts
@@ -8,11 +8,11 @@ const sendDefaults = {
 };
 
 // FIXME: Add your production URL
-const productionURL = 'https://test-checkly2.vercel.app';
+const productionURL = 'https://boilerplate.lumineau.app';
 
 const emailChannel = new EmailAlertChannel('email-channel-1', {
   // FIXME: add your own email address, Checkly will send you an email notification if a check fails
-  address: 'contact@creativedesignsguru.com',
+  address: 'lumineau.corentin11@gmail.com',
   ...sendDefaults,
 });
 
